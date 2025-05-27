257cc26dd3d5e0f965486ebb0db011e1a095cf0e
julianty <alexanderjulianty@gmail.com>
2025-05-26 17:32:22 -0700

    Change to fit schema type

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,19 +1,21 @@
-export const restaurantMockData = [
+import type { Restaurant } from "../../generated/prisma";
+
+export const restaurantMockData: Restaurant[] = [
   {
-    id: 1,
+    id: "1",
     name: "In-N-Out Burger",
     description: "Fast food chain known for its burgers, fries, and shakes.",
     imageUrl: "/in-n-out-burger.jpg",
   },
   {
-    id: 2,
+    id: "2",
     name: "Chipotle Mexican Grill",
     description:
       "Fast casual restaurant chain specializing in burritos and tacos.",
     imageUrl: "/chipotle-logo.png",
   },
   {
-    id: 3,
+    id: "3",
     name: "Panda Express",
     description:
       "American fast casual restaurant chain that serves American Chinese cuisine.",
