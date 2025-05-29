7e566b4bd30a8899cef3a924ef8d41c8a09486b7
julianty <alexanderjulianty@gmail.com>
2025-05-29 12:19:11 -0700

    Add layout components to SessionPage for improved user experience

--- src/app/sessions/[sessionid]/page.tsx
+++ src/app/sessions/[sessionid]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { Container, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 
 interface SessionPageProps {
@@ -20,6 +21,16 @@ export default async function SessionPage({ params }: SessionPageProps) {
 
   return (
     <main className="p-8">
+      <Container>
+        <Flex direction={"column"} align={"center"}>
+          <h1 className="text-4xl m-4">What&apos;s for Dinner?</h1>
+          <p>
+            You&apos;ve just been invited to help make a decision on dinner
+            plans
+          </p>
+          <p>Please see the options that were selected below</p>
+        </Flex>
+      </Container>
       <h1 className="text-2xl font-bold mb-4">Session: {session.id}</h1>
       <pre className="p-4 rounded overflow-x-auto">
         {JSON.stringify(session, null, 2)}
