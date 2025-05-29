24ab49420de87677ba4b55439dfb577a744a824f
julianty <alexanderjulianty@gmail.com>
2025-05-29 16:31:51 -0700

    Enhance RestaurantSelector with user feedback and session creation functionality

--- src/ui/components/RestaurantSelector.tsx
+++ src/ui/components/RestaurantSelector.tsx
@@ -63,12 +63,21 @@ export default function RestaurantSelector({
   return (
     <>
       <Section>
-        <Flex wrap="wrap" gap="3">
+        <div className="my-4">
           <p>
             Let&apos;s get started by selecting a restaurant from the list
             below. You can add your own restaurants by clicking the &quot;Add
             Restaurant&quot; button.
           </p>
+        </div>
+        <Flex
+          gap="3"
+          direction={"column"}
+          justify={"center"}
+          maxWidth={"500px"}
+          mx={"auto"}
+          my={"8"}
+        >
           {restaurants
             .filter((restaurant) => {
               return (
@@ -90,7 +99,6 @@ export default function RestaurantSelector({
         </Flex>
         <form onSubmit={handleCustomOptionSubmit}>
           <p>If you want to write in your own options, you can do that here!</p>
-          {/* <input type="text"></input> */}
           <TextField.Root
             placeholder="Your favorite restaurant/food"
             className="max-w-96"
@@ -103,6 +111,53 @@ export default function RestaurantSelector({
           <Button type="submit">Add Option</Button>
         </form>
       </Section>
+      <Section>
+        {selectedRestaurants.length > 0 && (
+          <>
+            <h2>
+              Here&apos;s what you&apos;ve selected so far, if it looks good,
+              you can hit the start session button to start!
+            </h2>
+            <ul>
+              {selectedRestaurants.map((r) => (
+                <li key={r.id}>{r.name}</li>
+              ))}
+            </ul>
+          </>
+        )}
+        {selectedRestaurants.length > 0 && (
+          <form action={createSessionAction}>
+            {selectedRestaurants
+              .filter((r) => r.userCreated == false)
+              .map((r) => (
+                <input
+                  key={r.id}
+                  type="hidden"
+                  name="restaurantIds"
+                  value={r.id}
+                />
+              ))}
+            {selectedRestaurants
+              .filter((r) => r.userCreated == true)
+              .map((r) => (
+                <input
+                  key={r.id}
+                  type="hidden"
+                  name="userEntries"
+                  value={r.id}
+                />
+              ))}
+            <Flex>
+              <TextField.Root placeholder="Enter your name" name="userName">
+                <TextField.Slot>{""}</TextField.Slot>
+              </TextField.Root>
+              <Button type="submit" className="mt-4">
+                Create Session
+              </Button>
+            </Flex>
+          </form>
+        )}
+      </Section>
       {/* Debug output */}
       <Section>
         <div className="bg-[#222] text-white p-3 rounded mt-4">
@@ -110,33 +165,6 @@ export default function RestaurantSelector({
           <pre className="whitespace-pre-wrap break-all">
             {JSON.stringify(selectedRestaurants, null, 2)}
           </pre>
-          {selectedRestaurants.length > 0 && (
-            <form action={createSessionAction}>
-              {selectedRestaurants
-                .filter((r) => r.userCreated == false)
-                .map((r) => (
-                  <input
-                    key={r.id}
-                    type="hidden"
-                    name="restaurantIds"
-                    value={r.id}
-                  />
-                ))}
-              {selectedRestaurants
-                .filter((r) => r.userCreated == true)
-                .map((r) => (
-                  <input
-                    key={r.id}
-                    type="hidden"
-                    name="userEntries"
-                    value={r.id}
-                  />
-                ))}
-              <Button type="submit" className="mt-4">
-                Create Session
-              </Button>
-            </form>
-          )}
         </div>
       </Section>
     </>
